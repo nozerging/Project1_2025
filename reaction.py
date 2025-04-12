b370975b026da5079a295791ac8deae2e0f2d5a1
a28682 <202283890021@nuist.edu.cn>
2025-04-12 05:05:05 +0100

    Added a loop

--- reaction.py
+++ reaction.py
@@ -5,20 +5,32 @@ from random import uniform
 led=LED(4)
 right_button=Button(15)
 left_button=Button(14)
-
 left_name=input('left player name is ')
 right_name=input('right player name is ')
 
-led.on()
-sleep(uniform(5,10))
-led.off()
-
 def pressed(button):
     if button.pin.number==14:
-        print(left_name+'won the game')
-    else:
-        print(right_name+'won the game')
-    exit()
+        print(left_name+' won the game')
+    elif button.pin.number==15:
+        print(right_name+' won the game')
+    right_button.when_pressed=None
+    left_button.when_pressed=None
+def scanner():
+    while(True):
+        right_button.wait_for_press(timeout=0.1)
+        left_button.wait_for_press(timeout=0.1)
+        if(left_button.is_pressed):
+            break
+        if(right_button.is_pressed):
+            break
 
-right_button.when_pressed=pressed
-left_button.when_pressed=pressed
+while(True):
+    led.on()
+    sleep(uniform(1,2))
+    led.off()
+    right_button.when_pressed=pressed 
+    left_button.when_pressed=pressed
+    scanner()
+    temp=input("Do you want to continue?(y/n)")
+    if(temp=='n' or temp=='no'):
+        exit()
